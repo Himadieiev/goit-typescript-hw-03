d9e0e61d0ccf67725f37820d49369e09f7128682
Ruslan Himadieiev <113462988+Himadieiev@users.noreply.github.com>
2023-10-08 12:51:43 +0300

    finish

--- src/4.ts
+++ src/4.ts
@@ -1,3 +1,68 @@
+class Key {
+    private signature: number;
+
+    constructor(){
+        this.signature = Math.random();
+    }
+
+    getSignature(): number {
+        return this.signature;
+    }
+}
+
+class Person {
+    private key: Key;
+
+    constructor(key: Key){
+        this.key = key;
+    }
+
+    getKey(): Key {
+        return this.key;
+    }
+}
+
+abstract class House {
+    protected door: boolean;
+    protected key: Key | null;
+    protected tenants: Person[];
+
+    constructor(key: Key) {
+       this.key = key;
+       this.door = false;
+       this.tenants = [];
+    }
+
+  abstract openDoor(key: Key): void;
+
+  comeIn(person: Person) {
+    if (this.door) {
+      this.tenants.push(person);
+      console.log("Person inside");
+    } else {
+      console.log("Person outside");
+    }
+  }
+}
+
+
+
+class MyHouse extends House {
+    constructor(key: Key) {
+    super(key);
+  }
+
+  openDoor(enteredKey: Key) {
+    if (enteredKey.getSignature() === this.key!.getSignature()) {
+      this.door = true;
+      console.log("The door is open");
+    } else {
+      console.log("Invalid key, the door isn't open");
+    }
+  }
+}
+
+
 const key = new Key();
 
 const house = new MyHouse(key);
